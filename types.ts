9a3d8ced2505339049d5043c30a02c5eac37d892
HenriqueYudyOnoOmoto <henriqueyudyonoomoto@gmail.com>
2025-05-30 06:59:52 +0900

    change to static html

--- types.ts
+++
@@ -1,9 +0,0 @@
-export interface SlideData {
-  id: string;
-  slideNumberText: string;
-  title: string;
-  mainParagraph?: string;
-  points: string[];
-  image?: string;
-  accentColor: string;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/noise-pollution-slides-main/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
